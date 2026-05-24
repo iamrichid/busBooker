e77bf82e90a2e17f278c0aac33e0207057751a1c
rich <rkklinks@gmail.com>
2026-05-24 14:41:03 +0000

    feat: Add function to format date and time for improved display

--- finance.js
+++ finance.js
@@ -463,6 +463,16 @@ function formatDateRange(fromDate, toDate) {
   return `${formatDate(fromDate)} to ${formatDate(toDate)}`;
 }
 
+function formatDateTime(value) {
+  return new Intl.DateTimeFormat("en-GB", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+  }).format(new Date(value));
+}
+
 function formatSlot(booking) {
   return `${booking.startTime || "--:--"} to ${booking.endTime || "--:--"}`;
 }
